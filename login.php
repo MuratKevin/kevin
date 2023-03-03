1b61d9015c8af99739b36ef267a1aeda026e9cdd
MuratKevin <126828869+MuratKevin@users.noreply.github.com>
2023-03-03 14:41:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   $to = "murat.kevin.idf@gmail.com";
   $subject = "Nouveau commentaire de $name";
   $body = "Nom : $name\n\nAdresse e-mail : $email\n\nMessage : $message";
-  $headers = "From: noreply@votre-site-web.com";
+  $headers = "From: noreply@votre-e-mail.com";
 
   if(mail($to, $subject, $body, $headers)) {
     echo "<p>Votre commentaire a été envoyé avec succès !</p>";
